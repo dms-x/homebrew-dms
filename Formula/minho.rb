a0abbc5cc6926144ffd91866c82d1ebfb74f0334
Your Name <singsangsong17@gmail.com>
2024-01-14 07:17:58 -0800

    jm

--- Formula/minho.rb
+++ Formula/minho.rb
@@ -1,7 +1,7 @@
 class Minho < Formula
   desc "makes icons for terminal"
   homepage "https://github.com/dms-x"
-  url "https://github.com/dms-x/homebrew-dms/archive/refs/tags/v1.0.zip"
+  url "https://github.com/dms-x/homebrew-dms/releases/download/v1.0/minho"
   sha256 "ee55a0511c6eff35965da30dc4e3d1d3b5229b379d2755593dc94b7f5c724680"
 
   def install
