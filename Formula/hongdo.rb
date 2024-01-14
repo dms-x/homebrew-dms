596fb128b162e6f8dcc8e5568ddda43c365365e1
Your Name <singsangsong17@gmail.com>
2024-01-14 15:24:18 -0800

    new hongdo V1

---
+++ Formula/hongdo.rb
@@ -0,0 +1,10 @@
+class Hongdo < Formula
+   desc "display image on terminal"
+   homepage "https://github.com/dms-x"
+   url "https://github.com/dms-x/homebrew-dms/releases/download/v1.0/hongdo"
+   sha256 "15038bce7ed8df967a44e2b4ccd8c0f52b61dc48f7e2849db2006ec8c79a8662"
+
+   def intsall
+     bin.install "hongdo"
+   end
+end
