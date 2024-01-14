1513a74a708c75dcaf313a80134c5d708c31c3da
Your Name <singsangsong17@gmail.com>
2024-01-14 06:53:54 -0800

    pls

--- Formula/minho.rb
+++ Formula/minho.rb
@@ -2,7 +2,7 @@ class Minho < Formula
   desc "makes icons for terminal"
   homepage "https://github.com/dms-x"
   url "https://github.com/dms-x/homebrew-dms/archive/refs/tags/v1.0.zip"
-  sha256 "ff0498fb67756279c845eccda97f6f7c82b649eca54f63a98730a795baf5e56b"
+  sha256 "ee55a0511c6eff35965da30dc4e3d1d3b5229b379d2755593dc94b7f5c724680"
 
   def install
     bin.install "minho"
