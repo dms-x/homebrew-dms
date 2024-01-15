6c64291c7111650ceece8598d3df8b67377dfa43
Your Name <singsangsong17@gmail.com>
2024-01-14 20:25:41 -0800

    install

--- Formula/hongdo.rb
+++ Formula/hongdo.rb
@@ -4,7 +4,7 @@ class Hongdo < Formula
    url "https://github.com/dms-x/homebrew-dms/releases/download/v1.0/hongdo"
    sha256 "15038bce7ed8df967a44e2b4ccd8c0f52b61dc48f7e2849db2006ec8c79a8662"
 
-   def intsall
+   def install
      bin.install "hongdo"
    end
 end
